bd6fbe2fb60871551059941ec6464fad3d9b5d25
Edson Eddy <crew0eddy@gmail.com>
2026-01-08 02:19:06 -0400

    Bump version to 1.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="csim",
-    version="1.1.0",
+    version="1.1.1",
     packages=find_packages(),
     install_requires=[
         "antlr4-python3-runtime",
